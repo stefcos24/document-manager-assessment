b1525cb5f3e7295899db11e1c30cc39d1d7622c9
stefcos24 <stefan22bl@gmail.com>
2026-05-28 15:08:21 +0200

    Register new urls

--- src/propylon_document_manager/site/urls.py
+++ src/propylon_document_manager/site/urls.py
@@ -1,26 +1,30 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 from propylon_document_manager.file_versions.api.views import (
+    DocumentRetrieveView,
     EmailObtainAuthToken,
     RegisterView,
 )
 
-# API URLS
 urlpatterns = [
     # Auth
     path("api/auth/register/", RegisterView.as_view(), name="register"),
     path("api/auth/login/", EmailObtainAuthToken.as_view(), name="login"),
 
-    # API base url
+    # File versions API
     path("api/", include("propylon_document_manager.site.api_router")),
+
+    # Document retrieval
+    path("api/documents/<path:file_url>", DocumentRetrieveView.as_view(), name="document-retrieve"),
 
     # DRF auth token
     path("api-auth/", include("rest_framework.urls")),
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
-
         urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
